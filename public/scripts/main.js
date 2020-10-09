4b409d9acecb4d582784a0dd00236eb2d92c0228
hayeonchoo <70540047+hayeonchoo@users.noreply.github.com>
2020-10-09 15:27:25 -0400

    lab 5

---
+++ public/scripts/main.js
@@ -0,0 +1,11 @@
+
+
+const btn = document.getElementById('activate');
+
+function bgChange() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.onclick = bgChange;
+btn.addEventListener("click", bgChange);
