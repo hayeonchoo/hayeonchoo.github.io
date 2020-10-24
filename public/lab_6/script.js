045171e0d136fee616ec2d6848d2773e4ce2f315
hayeonchoo <70540047+hayeonchoo@users.noreply.github.com>
2020-10-23 22:56:21 -0400

    lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,10 @@
 // You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min,max){
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random()*(max1-min1+1)+min1);
 
+}
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -8,10 +13,10 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
-  if (a[key] < b[key]) {
+function sortFunction(org, comp, key) {
+  if (org[key] < comp[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  } if (org[key] > comp[key]) {
     return 1;
   }
   return 0;
@@ -29,7 +34,29 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
+      if (document.querySelector('.flex-inner')){
+        document.querySelector('.flex-inner').remove();
+      }
+      const arr = range(10);
+      const arrr = arr.map(() =>{
+        const nmb = getRandomIntInclusive(0,243);
+        return fromServer[nmb];
+
+      })
+
+      const reverseList = arr.sort((org,comp) => sortByKey(org,comp,'name'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex-inner';
+      $('form').prepend(ol);
+
+      reverseList.forEach((el, i)=>{
+        const li = document.createElement('li');
+        $(li).append('<input type = "checkbox" value = ${el.code} id = ${el.code}/>');
+        $(li).append('<label for = ${el.code}>${el.name}</label>');
+        $(ol).append(li);
+
+      })
+
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
